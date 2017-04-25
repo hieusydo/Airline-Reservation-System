085c2d5489bdd41e0b7e707110e6b7017f205ad6
hieusydo <hieusydo@gmail.com>
2017-04-25 16:31:36 -0400

    Fix untracked files.

--- appdef.py
+++ appdef.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 #Configure MySQL
 conn = pymysql.connect(host='localhost',
                        user='root',
-                       password='',
+                       password='root',
                        db='airline-booking',
+                       port=8889,
                        # unix_socket='/Applications/MAMP/tmp/mysql/mysql.sock',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
--- queries.sql
+++ queries.sql
@@ -12,3 +12,5 @@ WHERE purchases.ticket_id = ticket.ticket_id
 AND ticket.airline_name = flight.airline_name AND ticket.flight_num = flight.flight_num
 AND purchases.booking_agent_id = booking_agent.booking_agent_id
 AND booking_agent.email = %s AND departure_time > curdate()
+
+-- Find flight based on arrival and departure city/time
