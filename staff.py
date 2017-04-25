4e02cea717326c42f98f7f84f03c13f66d2ff438
NotMichaelChen <mzc12345@gmail.com>
2017-04-25 18:04:35 -0400

    Added staff authentication

--- staff.py
+++ staff.py
@@ -3,7 +3,24 @@ import pymysql.cursors
 
 from appdef import app, conn
 
+#Make sure that the user is actually staff before performing any operations
+def authenticateStaff():
+    username = session['username']
+    cursor = conn.cursor()
+    query = 'select * from airline_staff where username=%s'
+    cursor.execute(query, (username))
+    data = cursor.fetchall()
+    cursor.close()
+    if(data):
+        return True
+    else:
+        return False
+
 @app.route('/staffHome')
 def staffHome():
-  return render_template('staff.html')
+    if authenticateStaff():
+        return render_template('staff.html')
+    else
+        error = 'Invalid Credentials'
+        return render_template('/', error=error)
 
