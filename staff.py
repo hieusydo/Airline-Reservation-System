8d0404be563a19fb8f3e7c4709e5551f975c19d3
NotMichaelChen <mzc12345@gmail.com>
2017-04-25 20:34:01 -0400

    Implemented flight status changing, added error page

--- staff.py
+++ staff.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import pymysql.cursors
 
-from appdef import app, conn
+from appdef import *
 
 #Make sure that the user is actually staff before performing any operations
 def authenticateStaff():
-    username = session['username']
+    username = ""
+    try:
+        #could be that there is no user, make sure
+        username = session['username']
+    except:
+        return False
+    
     cursor = conn.cursor()
     query = 'select * from airline_staff where username=%s'
     cursor.execute(query, (username))
@@ -14,13 +20,45 @@ def authenticateStaff():
     if(data):
         return True
     else:
+        #Logout before returning error message
+        session.pop('username')
         return False
 
 @app.route('/staffHome')
 def staffHome():
     if authenticateStaff():
         return render_template('staff.html')
-    else
+    else:
         error = 'Invalid Credentials'
-        return render_template('/', error=error)
+        return redirect(url_for('errorpage', error=error))
 
+@app.route('/staffHome/changeFlight')
+def changeFlightStatusPage():
+    if authenticateStaff():
+        return render_template('changeFlight.html')
+    else:
+        error = 'Invalid Credentials'
+        return redirect(url_for('errorpage', error=error))
+
+@app.route('/staffHome/changeFlight/Auth', methods=['POST'])
+def changeFlightStatus():
+    if not authenticateStaff():
+        error = 'Invalid Credentials'
+        return redirect(url_for('errorpage', error=error))
+    
+    username = session['username']
+    
+    flightnum = request.form['flightnum']
+    status = request.form['status']
+    if not status:
+        error = 'Did not select new status'
+        return render_template('staffHome/changeFlight', error=error)
+    
+    cursor = conn.cursor()
+    query = 'update flight set status=%s where flight_num=%s'
+    cursor.execute(query, (status, flightnum))
+    conn.commit()
+    cursor.close()
+    
+    return render_template('staff.html', message="Operation Successful", username=username)
+    
