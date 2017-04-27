c03c270c9ddc7b34bea6bfcde38e316b74debbe1
hieusydo <hieusydo@gmail.com>
2017-04-27 13:42:27 -0400

    Fixed bug in searchPurchase. Added purchase for agents.

--- agent.py
+++ agent.py
@@ -7,12 +7,13 @@ from appdef import app, conn
 def agentHome():
   username = session['username']
   cursor = conn.cursor();
-  query = 'SELECT purchases.ticket_id, ticket.airline_name, ticket.flight_num, departure_airport, departure_time, arrival_airport, arrival_time \
-  FROM purchases, ticket, flight \
+  query = 'SELECT * \
+  FROM purchases, ticket, flight, booking_agent \
   WHERE purchases.ticket_id = ticket.ticket_id \
   AND ticket.airline_name = flight.airline_name \
   AND ticket.flight_num = flight.flight_num \
-  AND customer_email = %s AND departure_time > curdate()'
+  AND booking_agent.email = %s AND booking_agent.booking_agent_id = purchases.booking_agent_id \
+  AND departure_time > curdate()'
   cursor.execute(query, (username))
   data = cursor.fetchall()
   cursor.close()  
--- purchase.py
+++ purchase.py
@@ -7,9 +7,12 @@ from appdef import app, conn
 def purchasePage():
   return render_template('purchase.html')
 
+@app.route('/purchasePageAgent')
+def purchasePageAgent():
+  return render_template('purchaseAgent.html')
+
 @app.route('/searchPurchase', methods=['POST'])
 def searchPurchase():
-  username = session['username']
   cursor = conn.cursor()
   fromcity = request.form['fromcity']
   fromairport = request.form['fromairport']
@@ -17,11 +20,9 @@ def searchPurchase():
   tocity = request.form['tocity']
   toairport = request.form['toairport']
   todate = request.form['todate']
-  query = 'SELECT * FROM flight as f, airport, purchases, ticket \
+  query = 'SELECT distinct f.airline_name, f.flight_num, departure_airport, departure_time, arrival_airport, arrival_time, price, airplane_id \
+          FROM flight as f, airport, purchases \
           WHERE airport.airport_name=f.departure_airport \
-          AND f.flight_num = ticket.flight_num AND f.airline_name = ticket.airline_name\
-          AND ticket.ticket_id = purchases.ticket_id\
-          AND purchases.customer_email = %s\
           AND airport.airport_city = %s \
           AND airport.airport_name = %s \
           AND f.departure_time BETWEEN %s AND %s \
@@ -37,7 +38,7 @@ def searchPurchase():
               >= (SELECT COUNT(*) \
               FROM ticket \
               WHERE ticket.airline_name = f.airline_name AND ticket.flight_num = f.flight_num)'
-  cursor.execute(query, (username, fromcity, fromairport, fromdate, todate, tocity, toairport))
+  cursor.execute(query, (fromcity, fromairport, fromdate, todate, tocity, toairport))
   data = cursor.fetchall()
   cursor.close()
   error = None
@@ -76,3 +77,77 @@ def purchase():
     #returns an error message to the html page
     error = 'Cannot complete purchase'
     return render_template('purchase.html', error=error)     
+
+@app.route('/searchPurchaseAgent', methods=['POST'])
+def searchPurchaseAgent():
+  cursor = conn.cursor()
+  fromcity = request.form['fromcity']
+  fromairport = request.form['fromairport']
+  fromdate = request.form['fromdate']
+  tocity = request.form['tocity']
+  toairport = request.form['toairport']
+  todate = request.form['todate']
+  query = 'SELECT distinct f.airline_name, f.flight_num, departure_airport, departure_time, arrival_airport, arrival_time, price, airplane_id \
+          FROM flight as f, airport, purchases \
+          WHERE airport.airport_name=f.departure_airport \
+          AND airport.airport_city = %s \
+          AND airport.airport_name = %s \
+          AND f.departure_time BETWEEN %s AND %s \
+          AND (f.airline_name, f.flight_num) in \
+            (SELECT flight.airline_name, flight.flight_num FROM flight, airport \
+            WHERE airport.airport_name=flight.arrival_airport \
+            AND airport.airport_city = %s \
+            AND airport.airport_name = %s) \
+          AND (SELECT DISTINCT seats \
+              FROM flight, airplane \
+              WHERE flight.airplane_id = airplane.airplane_id AND flight.airline_name = airplane.airline_name \
+              AND flight.airline_name = f.airline_name AND flight.flight_num = f.flight_num) \
+              >= (SELECT COUNT(*) \
+              FROM ticket \
+              WHERE ticket.airline_name = f.airline_name AND ticket.flight_num = f.flight_num)'
+
+  cursor.execute(query, (fromcity, fromairport, fromdate, todate, tocity, toairport))
+  data = cursor.fetchall()
+  cursor.close()
+  error = None
+  if(data):
+    print(data)
+    return render_template('purchaseAgent.html', results=data)
+  else:
+    #returns an error message to the html page
+    error = 'No results found'
+    return render_template('purchaseAgent.html', error=error) 
+
+@app.route('/purchaseAgent', methods=['POST'])
+def purchaseAgent():
+  username = session['username']
+  customer_email = request.form['customer_email']
+  cursor = conn.cursor()
+  airline_name = request.form['airline_name']
+  flight_num = request.form['flight_num']
+  # Find the number of tickets to generate the next ticket_id
+  queryCount = 'SELECT COUNT(*) as count FROM ticket \
+                WHERE ticket.airline_name = %s AND ticket.flight_num = %s'
+  cursor.execute(queryCount, (airline_name, flight_num))
+  ticketCount = cursor.fetchone()
+  ticket_id = ticketCount['count'] + 1
+  # Create the new ticket
+  queryNewTicket = 'INSERT INTO ticket VALUES(%s, %s, %s)'
+  cursor.execute(queryNewTicket, (ticket_id, airline_name, flight_num))
+  # Get booking_agent_id
+  queryGetID = 'SELECT booking_agent_id FROM booking_agent WHERE email=%s'
+  cursor.execute(queryGetID, username)
+  agentID = cursor.fetchone() # returns a dict 
+  # Finalize the purchase
+  queryPurchase = 'INSERT INTO purchases VALUES(%s, %s, %s, CURDATE())'
+  cursor.execute(queryPurchase, (ticket_id, customer_email, agentID['booking_agent_id']))
+  data = cursor.fetchone()
+  conn.commit()
+  cursor.close()
+  error = None
+  if(data):
+    return render_template('agent.html', results=data)
+  else:
+    #returns an error message to the html page
+    error = 'Cannot complete purchase'
+    return render_template('purchaseAgent.html', error=error)        
